c6eb70c1e8ea9cfc60dbf1a5bca9a152ae6e68eb
Ismar Muslić <ismar.muslic@outlook.com>
2025-09-22 21:38:38 +0200

    Login and register BE works

--- backend/server.js
+++ backend/server.js
@@ -17,14 +17,6 @@ app.get('/api/hello', (req, res) => {
 	res.json({ message: 'Hello from Express backend!' });
 });
 
-// login route
-app.post('/api/login', (req, res) => {
-	const { username, password } = req.body;
-	console.log('Login attempt:', { username, password });
-	// Here you would normally check the username and password against your database
-	res.json({ message: 'Login successful' });
-});
-
 // register route
 app.post('/api/register', async (req, res) => {
 	try {
@@ -41,6 +33,35 @@ app.post('/api/register', async (req, res) => {
 	} catch (error) {
 		console.error('Error registering user:', error);
 		res.status(500).json({ message: 'Error registering user' });
+	}
+});
+
+// login route
+app.post('/api/login', async (req, res) => {
+	try {
+
+		const { username, password } = req.body;
+		console.log('Login attempt:', { username, password });
+		
+		const user = await User.findOne({ where: { username } });
+		if (!user) {
+			return res.status(400).json({ message: 'Invalid username or password' });
+		}
+
+		const passwordMatch = await bcrypt.compare(password, user.password);
+		if (!passwordMatch) {
+			return res.status(400).json({ message: 'Invalid username or password' });
+		}
+		// If 2FA is enabled, you would typically check the 2FA token here
+		// For simplicity, we skip that step in this example
+
+		// Successful login
+
+
+		res.json({ message: 'Login successful', user: user.username });
+	} catch (error) {
+		console.error('Error during login:', error);
+		res.status(500).json({ message: 'Error during login' });
 	}
 });
 
